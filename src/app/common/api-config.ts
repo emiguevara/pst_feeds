9120b950dd4518e9ef45fbd83f62137bec031506
Emiliano Guevara <emiliano.guevara@opoint.com>
2017-02-23 12:58:06 +0100

    Point to https://api-dev.opoint.com

--- src/app/common/api-config.ts
+++ src/app/common/api-config.ts
@@ -1,9 +1,18 @@
 import {StorageKeys} from './storage-keys';
 import {AuthConfig} from 'angular2-jwt';
+import {Headers} from '@angular/http';
 
 export class ApiConfig {
-  static AUTH_PATH = '//api-emiliano.opoint.com/auth/get-token/';
-  static PATH = '//api-emiliano.opoint.com/play/';
+  static AUTH_PATH = 'https://api-dev.opoint.com/auth/get-token/';
+  static PATH = 'https://api-dev.opoint.com/play/';
+
+  static HEADERS = (() => {
+    const requestHeaders = new Headers();
+    requestHeaders.append('Content-Type', 'application/json');
+    requestHeaders.append('Accept', 'application/json');
+
+    return {headers: requestHeaders};
+  })();
 
   static AUTH_FACTORY () {
     return new AuthConfig({
